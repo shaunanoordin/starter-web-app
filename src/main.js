28d9e91b5b40602873c51cb72826b99d1d04e011
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2022-12-09 22:24:14 +0800

    Dec 2022 Update (#20)
    * Update dependencies for Dec 2022
    
    * Add Webpack aliases

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/*  
+/*
 Starter JS
 ----------
 
@@ -7,7 +7,7 @@ Starter template for JavaScript web app projects.
 (Shaun A. Noordin | shaunanoordin.com | 20180413)
  */
 
-import {ImportExample} from "./importExample.js";
+import { ImportExample } from "@src/importExample.js";
 
 /*  Primary App Class
  */
@@ -15,7 +15,7 @@ import {ImportExample} from "./importExample.js";
 class App {
   constructor() {
     let importExample = new ImportExample("HI THERE");
-    
+
     this.console = document.getElementById("console");
     this.console.innerHTML =
       "This is a starter template for JS projects. <br>" +
--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,9 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    }
+  },
 }
